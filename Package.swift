369638f4ee4205c94a247ddc4766173213348e20
gaetanomatonti <gaetanomatonti@icloud.com>
2024-10-12 15:12:08 +0200

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "DesignTokens",
+  platforms: [
+    .macOS(.v11)
+  ],
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "DesignTokens",
+      targets: ["DesignTokens"]
+    ),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "DesignTokens"
+    ),
+    .testTarget(
+      name: "DesignTokensTests",
+      dependencies: ["DesignTokens"],
+      resources: [
+        .copy("Resources/color_token.json"),
+        .copy("Resources/groups.json"),
+      ]
+    ),
+  ]
+)
---
+++ README.md
@@ -0,0 +1,4 @@
+#  DesignTokens
+
+DesignTokens is a command line tool to generate Swift source code from a design token JSON file.
+This package follows the [W3C Design Token standard](https://tr.designtokens.org/format/#:~:text=The%20names%20of%20the%20groups,which%20is%20a%20computed%20property).
